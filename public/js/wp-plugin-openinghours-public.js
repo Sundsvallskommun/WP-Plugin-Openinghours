3b7f8665b03ba22c415608bf027b2526be0fe391
Daniel Söderström <daniel.soderstrom@cybercom.com>
2016-12-01 15:56:35 +0100

    jQuery conflict fix

--- public/js/wp-plugin-openinghours-public.js
+++ public/js/wp-plugin-openinghours-public.js
@@ -49,39 +49,40 @@
 
 	} );
 
+
+	function load_opening_hours( date ) {
+
+		var location = $('.current-location').html();
+
+		$('.current-date').html( date );
+		$('.loader').show();
+		$('.opening-hours-widget .list-group-item').fadeTo('medium', 0.2);
+
+		$.post( WP_PLUGIN_OPENINGHOURS.ajax_url, {
+
+			'action': 'load_opening_hours',
+			'nonce': WP_PLUGIN_OPENINGHOURS.ajax_nonce,
+			'date': date,
+			'location': location
+
+		}, function( response ) {
+
+			$('.opening-hours-header .header .date').empty().append(response.date);
+			$('.loader').hide();
+			$('.opening-hours-widget').replaceWith( response.hours );
+
+		});
+	}
+
+	function getFormattedDate( date ) {
+		var str = date.getFullYear() + "-" + getFormattedPartTime(date.getMonth()+1) + "-" + getFormattedPartTime(date.getDate());
+		return str;
+	}
+
+	function getFormattedPartTime(partTime){
+		if (partTime<10)
+			return "0"+partTime;
+		return partTime;
+	}
+
 })( jQuery );
-
-function load_opening_hours( date ) {
-
-	var location = $('.current-location').html();
-
-	$('.current-date').html( date );
-	$('.loader').show();
-	$('.opening-hours-widget .list-group-item').fadeTo('medium', 0.2);
-
-	$.post( WP_PLUGIN_OPENINGHOURS.ajax_url, {
-
-		'action': 'load_opening_hours',
-		'nonce': WP_PLUGIN_OPENINGHOURS.ajax_nonce,
-		'date': date,
-		'location': location
-
-	}, function( response ) {
-
-		$('.opening-hours-header .header .date').empty().append(response.date);
-		$('.loader').hide();
-		$('.opening-hours-widget').replaceWith( response.hours );
-
-	});
-}
-
-function getFormattedDate( date ) {
-	var str = date.getFullYear() + "-" + getFormattedPartTime(date.getMonth()+1) + "-" + getFormattedPartTime(date.getDate());
-	return str;
-}
-
-function getFormattedPartTime(partTime){
-	if (partTime<10)
-		return "0"+partTime;
-	return partTime;
-}
